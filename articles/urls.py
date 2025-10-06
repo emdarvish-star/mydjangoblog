8ba80731c480ea88d1c7cddd902ef9c16e996632
masoumehdarvish <em.darvish@gmail.com>
2025-10-06 12:56:28 +0330

    update link article

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = "articles"
 urlpatterns = [
-    path('',views.articles_list),
-    path('<slug>',views.article_detail),
+    path('',views.articles_list, name ="List"),
+    path('<slug>',views.article_detail,name = "detail"),
 ]
